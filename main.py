2d8ba322155523be6415f3c575a649d78ae66a69
lucsaito2 <87723915+lucsaito2@users.noreply.github.com>
2022-09-26 20:46:29 -0300

    Codigo proprio adicionado

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import random
 # Declare suas funções abaixo
+def f():
+    pass
 
 
 if __name__ == "__main__":
     # Adicione seu código abaixo e declare suas funções a serem usadas acima
-    print("Código a ser executado!")
+    print("Outra linha de codigo")
