5dd5f2dd8a976c3c5a6a3b2df00bc7c31ef0ed76
Tiago Lima <stoutiago@gmail.com>
2022-09-26 18:01:30 -0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import random
-# Declare suas funções abaixo
-
-
-if __name__ == "__main__":
-    # Adicione seu código abaixo e declare suas funções a serem usadas acima
-    print("Código a ser executado!")
