b051bc02a528f300ef97b2331fdfc057ce71c0da
Jacqueline Nascimento <68693824+jacquelinenascimento@users.noreply.github.com>
2022-09-28 09:27:43 -0300

    projeto_modulo_1
    Minha resolução do projeto para finalização do módulo 1 de Python do Diversidade Tech, Ada & Suzano

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 import random
-# Declare suas funções abaixo
+palavras = ['UVA', 'BANANA', 'CACAU', 'MANGA', 'LARANJA', 'GOIABA', 'PITAYA', 'ABACAXI', 'KIWI', 'ABACATE', 'CEREJA']
+palavra = random.choice(palavras)
 
+tentativas = []
+contador = 0
+resultado = True
 
-if __name__ == "__main__":
-    # Adicione seu código abaixo e declare suas funções a serem usadas acima
-    print("Código a ser executado!")
+while(contador < 5):
+  palpite = input(f"Digite uma letra ou 0 para encerrar os palpites. Você tem {5 - contador} tentativas: ").upper()
+  if palpite == '0':
+    break
+  else:
+    tentativas.append(palpite)
+    impressao = ""
+    for l in palavra:
+      if(l in tentativas):
+        impressao += l
+      else:
+        impressao += "_"
+    print(impressao)
+    contador += 1
+
+if (impressao == palavra):
+  resultado = True
+elif (impressao != palavra):
+  resultado = False
+
+if (resultado == True):
+  print('Parabéns, você acertou! A palavra é ' + impressao)
+elif (resultado == False):
+  print('Que pena! Você perdeu!')
+else:
+  pass
