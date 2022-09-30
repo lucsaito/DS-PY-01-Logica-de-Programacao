43205bafab386b7c58f08bd2f95a3c57f77f1d36
luiswictor <114777378+luiswictor@users.noreply.github.com>
2022-09-30 17:54:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
-import random
-# Declare suas funções abaixo
+from random import choice
 
+vocabulario = ['arvore', 'banana', 'gato', 'churrasqueira']
 
-if __name__ == "__main__":
-    # Adicione seu código abaixo e declare suas funções a serem usadas acima
-    print("Código a ser executado!")
+palavra = choice(vocabulario)
+
+print("DESAFIO MODULO 1 HANGMAN (Jogo da Forca)\n")
+print("JOGO DA FORCA. Boa sorte!\n")
+
+chances = 3
+alfabeto = list("abdcefghijklmnopqrstuvwxyz")
+tentativas = []
+
+while True:
+  print(tentativas)
+  print("Chances: ",chances)
+
+  for letra in palavra:
+    if letra in tentativas:
+      print(letra, end = ' ')
+    else:
+      print('_', end= ' ')
+
+  palpite = input("\nDigite uma letra ").lower()
+  if palpite not in alfabeto or palpite == '':
+    print("Digite um caractere válido")
+    continue	
+  elif palpite in tentativas:
+    print("ih rapaz, tu já escreveu essa letra. Tenta outra!")
+    continue
+  tentativas.append(palpite)
+  if palpite in palavra:
+    print("Boa!")
+  else:
+    print("Rapazzz!")
+    chances-=1
+  if chances == 0:
+    print("Suas tentativas acabaram :(")
+    break
+  elif set(palavra).issubset(set(tentativas)):
+    palavra = palavra.upper()
+    print(palavra)
+    print("Parabéns! Você descobriu a palavra.")
+    break
